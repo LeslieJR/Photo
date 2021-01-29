d48c2d6cf57fb97f90599c21ab8274b2f8efa872
Leslie Reyes <50947842+LeslieJR@users.noreply.github.com>
2021-01-29 09:45:50 +0100

    form to upload new images updated

--- src/components/AddPhoto.js
+++ src/components/AddPhoto.js
@@ -1,10 +1,18 @@
 import React, {Component} from 'react';
+import Title from './Title';
 
 class AddPhoto extends Component{
     render(){
         return(
             <div>
-                <p>Add Photo</p>
+                <Title title={'Photogram'}/>
+                <div className='form'>
+                    <form>
+                        <input type='text' placeholder='Link'/>
+                        <input type='text' placeholder='Description'/>
+                        <button>POST</button>
+                    </form>
+                </div>
             </div>
         )
     }
